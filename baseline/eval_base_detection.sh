3e18b039ef601e6d7c29b8eaf4a231cb7fff3d13
zhangyuqing <zhangyuqing.pkusms@gmail.com>
2021-07-23 16:25:47 -0700

    add performance eval of fune tuning

--- baseline/eval_base_detection.sh
+++ baseline/eval_base_detection.sh
@@ -2,6 +2,6 @@
 cd /Users/yuqingz/GoogleDrive/2021_MLEbootcamp/2_capstone/autonomous_driving/baseline
 
 python ./argoverse-api/argoverse/evaluation/detection/eval.py \
-        --dt_fpath /Users/yuqingz/Documents/Argo/argoverse_detections_2020_train4 \
-        --gt_fpath /Users/yuqingz/Documents/Argo/argoverse-tracking/train4 \
-        -f eval
+        --dt_fpath /Users/yuqingz/Documents/Argo/argoverse_detections_2020_train4_trn \
+        --gt_fpath /Users/yuqingz/Documents/Argo/argoverse-tracking-v3/train4 \
+        -f ./
